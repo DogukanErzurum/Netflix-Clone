b8c41c0bc5f5b9a6b45eb2e0db5b6eaabeb9ef15
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-08-14 15:44:19 +0300

    The navbar part is complete.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next"
+import { getSession } from "next-auth/react"
 
-import { getSession, signOut } from "next-auth/react"
+import Navbar from "@/components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,13 +21,42 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
-       <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-       <p className="text-white">Logged in as : {user?.name}</p>
-       <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
+      <Navbar />
+      <div className="bg-gray-500">
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      </div>
     </>
   )
 }
