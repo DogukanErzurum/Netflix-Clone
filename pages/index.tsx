70dd860340e86727c5432dddc04808dd95e47f9b
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-08-18 12:58:39 +0300

    randomly bringing the animations of the movies to the billboard screen.

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,39 +24,6 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="bg-gray-500">
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      <div className="h-96"></div>
-      </div>
     </>
   )
 }
